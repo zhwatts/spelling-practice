bdcd7c61c83691cfebb07d967bef3580898935b9
Zach Watts <zach@zachwatts.online>
2024-09-10 18:24:06 -0400

    Adds ability to update list name by clicking on the header item

--- src/pages/ListContent/ListContent.tsx
+++ src/pages/ListContent/ListContent.tsx
@@ -6,18 +6,107 @@ import {
 } from "@mui/material";
 import { IWordList } from "../../interfaces/IWordList";
 import { AddCircleOutline } from "@mui/icons-material";
+import useSpellingLists from "../../hooks";
+import { Controller, useForm } from "react-hook-form";
+import { useEffect, useState } from "react";
 
-export function ListContent({ focusedList }: { focusedList?: IWordList }) {
-  return (
-    <Grid container direction="column" flexGrow={2} spacing={1}>
+interface EditableSpellingListTitleProps {
+  listId: number;
+}
+
+const SpellingListTitle: React.FC<EditableSpellingListTitleProps> = ({
+  listId,
+}) => {
+  const { spellingLists, editSpellingList } = useSpellingLists();
+  const selectedList = spellingLists.find((list) => list.id === listId);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const {
+    control,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    defaultValues: { title: selectedList?.title ?? "" },
+  });
+
+  useEffect(() => {
+    if (selectedList) {
+      reset({ title: selectedList.title });
+    }
+  }, [selectedList, reset]);
+
+  if (!selectedList) {
+    return (
       <Typography
         ml="34px"
         component="h1"
         variant="h4"
         color="primary.contrastText"
       >
-        {focusedList ? focusedList.title : "Create a new list"}
+        Spelling List Not Found
       </Typography>
+    );
+  }
+
+  const onSubmit = (data: { title: string }) => {
+    if (data.title.trim() === "") {
+      reset({ title: selectedList.title });
+    } else {
+      editSpellingList(listId, { ...selectedList, title: data.title });
+    }
+    setIsEditing(false);
+  };
+
+  return (
+    <div>
+      {isEditing ? (
+        <form onBlur={handleSubmit(onSubmit)} onSubmit={handleSubmit(onSubmit)}>
+          <Controller
+            name="title"
+            control={control}
+            render={({ field }) => (
+              <TextField
+                {...field}
+                autoFocus
+                variant="standard"
+                fullWidth
+                size="small"
+                sx={{ p: 0, px: "34px", m: 0 }}
+                slotProps={{
+                  input: {
+                    sx: {
+                      fontSize: "2.125rem",
+                      padding: 0,
+                      my: "-5px",
+                    },
+                  },
+                }}
+                error={!!errors.title}
+              />
+            )}
+          />
+        </form>
+      ) : (
+        <Typography
+          ml="34px"
+          component="h1"
+          variant="h4"
+          color="primary.contrastText"
+          onClick={() => setIsEditing(true)}
+          sx={{ cursor: "pointer" }}
+        >
+          {selectedList.title}
+        </Typography>
+      )}
+    </div>
+  );
+};
+
+export function ListContent({ focusedList }: { focusedList?: IWordList }) {
+  return (
+    <Grid container direction="column" flexGrow={2} spacing={1}>
+      <SpellingListTitle listId={focusedList?.id ?? 1} />
 
       <TextField
         fullWidth
